3900e9af16e6a119c91876a5f6a1ba1ef16ca0e2
Bianca Barbosa <yume_shiro@hotmail.com>
2021-06-25 11:38:13 -0300

    adj exception handler

--- src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
@@ -5,9 +5,10 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class TopicoForm(
-        @field:NotEmpty @field:Size(min = 5, max = 100)
+        @field:NotEmpty(message = "Mensagem nao pode ser em branco")
+        @field:Size(min = 5, max = 100, message = "Titulo deve ter entre 5 e 100 caracteres")
         val title: String,
-        @field:NotEmpty
+        @field:NotEmpty(message = "Mensagem nao pode ser em branco")
         val message: String,
         @field:NotNull
         val idCourse: Long,
--- src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.exception
 
 import br.com.alura.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -38,4 +39,21 @@ class ExceptionHandler {
         )
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+            exception: MethodArgumentNotValidException,
+            request: HttpServletRequest
+    ) : ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach {
+            e -> errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+                status = HttpStatus.BAD_REQUEST.value(),
+                error = HttpStatus.BAD_REQUEST.name,
+                message = errorMessage.toString(),
+                path = request.servletPath
+        )
+    }
 }
